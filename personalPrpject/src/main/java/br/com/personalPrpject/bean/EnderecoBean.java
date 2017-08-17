1bb741d8ea1a3960126537cd66876e09c20ca531
Alerson <alerson.rigo@gmail.com>
2017-08-17 18:07:29 -0300

    Criação do bean responsável pela comunicação entre a tela tabelaEnderecos.xhtml e o webservice.

---
+++ personalPrpject/src/main/java/br/com/personalPrpject/bean/EnderecoBean.java
@@ -0,0 +1,15 @@
+package br.com.personalPrpject.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "enderecoBean")
+public class EnderecoBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3817194591110827981L;
+
+}
