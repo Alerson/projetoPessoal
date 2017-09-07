e4439c146c4650d0a58b4317e1408a2c168a9cf1
Alerson <alerson.rigo@gmail.com>
2017-09-07 05:11:32 -0300

    Implementação do envio de e-mail.

--- personalPrpject/src/main/java/br/com/personalPrpject/service/impl/EmailServiceImpl.java
+++ personalPrpject/src/main/java/br/com/personalPrpject/service/impl/EmailServiceImpl.java
@@ -1,26 +1,13 @@
 package br.com.personalPrpject.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import br.com.personalPrpject.service.EmailService;
-import br.com.personalPrpject.util.EnviarEmail;
+import br.com.personalPrpject.util.EnviaEmail;
 
-@Service
 public class EmailServiceImpl implements EmailService {
-
-	private static final String EMAIL_NOTIFICACAO = "alerson.rigo@gmail.com";
-
-	@Autowired
-	private EnviarEmail email;
 
 	@Override
 	public void enviar(String assunto, String mensagem, String enviadoPor) {
-		try {
-			email.enviarEmail(assunto, mensagem, EMAIL_NOTIFICACAO, enviadoPor);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		EnviaEmail.enviarHTMLEmail(assunto, mensagem, enviadoPor);
 	}
 
 }
---
+++ personalPrpject/src/main/java/br/com/personalPrpject/util/EnviaEmail.java
@@ -0,0 +1,59 @@
+package br.com.personalPrpject.util;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.HtmlEmail;
+import org.apache.commons.mail.SimpleEmail;
+
+/**
+ * Classe Utilitária que contém métodos para envio de Email através de conta do
+ * Gmail
+ */
+public class EnviaEmail {
+
+	private static final String PASSWORD = "xxxxx";
+	private static final String USER = "xxxx.xxxx@gmail.com";
+	private static final String PORT_GMAIL_COM = "465";
+	private static final String SMTP_GMAIL_COM = "smtp.gmail.com";
+
+	public static void enviarEmail(String assunto, String mensagem, String emailPessoal) {
+		SimpleEmail email = new SimpleEmail();
+		email.setSSLOnConnect(true);
+		email.setHostName(SMTP_GMAIL_COM);
+		email.setSslSmtpPort(PORT_GMAIL_COM);
+		email.setAuthenticator(new DefaultAuthenticator(USER, PASSWORD));
+		try {
+			email.setFrom(emailPessoal);
+			email.setDebug(true);
+			email.setSubject(assunto);
+			email.setMsg(mensagem);
+			email.addTo(USER);
+			email.send();
+		} catch (EmailException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void enviarHTMLEmail(String assunto, String mensagem, String emailPessoal) {
+		HtmlEmail email = new HtmlEmail();
+		email.setSSLOnConnect(true);
+		email.setHostName(SMTP_GMAIL_COM);
+		email.setSslSmtpPort(PORT_GMAIL_COM);
+		email.setAuthenticator(new DefaultAuthenticator(USER, PASSWORD));
+		try {
+			email.setFrom(emailPessoal);
+			email.setDebug(true);
+			email.setSubject(assunto);
+			StringBuilder builder = new StringBuilder();
+			builder.append("<h3>Buscador CEP</h3>");
+			builder.append("<p>" + mensagem + "</p>");
+			builder.append("<p>Enviado por: " + emailPessoal + "</p>");
+			email.setHtmlMsg(builder.toString());
+			email.addTo(USER);
+			email.send();
+		} catch (EmailException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
